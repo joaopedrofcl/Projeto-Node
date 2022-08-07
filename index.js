e0b2e7df4e17945afe0e99b2f6e536f626f0e550
João Pedro Fernandes <joaopedrofclol@gmail.com>
2022-08-07 20:32:19 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
+///importando o modulo express
 const express = require("express")
+// atribuindo a uma variavel o module express
 const app = express()
 
+// criando uma rota raiz para a nossa aplicação
 app.get("/" , function(req,res){
     res.send('<h1>olá mundo</h1>')
 }
 )
+
 
 app.listen(8081 , function(){
     console.log('servidor rodando na porta http://localhost:8081')
