107904d8bda8b03d4373c5bd0d4297ce4a880a4d
João Pedro Fernandes <joaopedrofclol@gmail.com>
2022-08-10 14:52:27 -0300

    Update teste.js

--- teste.js
+++ teste.js
@@ -10,11 +10,27 @@ const sequelize = new Sequelize("teste" , "root" , senhamysql , {
 })
 
 /* o comando squelize.authenticate() serve para conectar a aplicação ao banco de dados
-o then() serve como se fosse uma função de callback assim que ele exxecutar o squelize.authenticate()
+o then() serve como se fosse uma função de callback assim que ele executar o squelize.authenticate()
 ele irá retornar  os parametros que estão dentro dele. o catch segue o mesmo principio porém retorna os parametros apenas se não for possivel a execução do comando sequelize.authenticate()
 */ 
+
+/*
 sequelize.authenticate().then(function(){
     console.log('Conectado ao banco de dados MYSQL')
 }).catch(function(erro){
     console.log("falha ao conectar"+erro)
 })
+*/
+
+//criação de models no sequelize
+
+const Postagem = sequelize.define('postagens' , {
+    titulo: {
+        type: Sequelize.STRING
+    }, 
+    conteudo: {
+        type: Sequelize.TEXT
+    }
+})
+
+Postagem.sync({force: true})
