06799da44a434e992b3d20d7e7ef38accce0c5a8
João Pedro Fernandes <joaopedrofclol@gmail.com>
2022-08-11 12:39:05 -0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const handlebars = require('express-handlebars')
+const bodyParser = require('body-parser')
+/*
+const mysqlsenha = require('./senha')
+const senhamysql = mysqlsenha.mysqlsenha
+*/
+const app  = express()
+// const Sequelize = require("sequelize") 
+
+/*
+const sequelize = new Sequelize("teste" , "root" , senhamysql , {
+    host: "localhost" ,
+    dialect: "mysql" 
+})
+*/
