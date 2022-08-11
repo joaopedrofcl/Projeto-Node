91be357c4b99ade4e5f82057cab1cd895db5fbdf
João Pedro Fernandes <joaopedrofclol@gmail.com>
2022-08-11 12:58:38 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
+//baixando os modulos da aplicação
 const express = require('express')
 const handlebars = require('express-handlebars')
 const bodyParser = require('body-parser')
-/*
-const mysqlsenha = require('./senha')
-const senhamysql = mysqlsenha.mysqlsenha
-*/
-const app  = express()
-// const Sequelize = require("sequelize") 
+//const Sequelize = require("sequelize")
 
-/*
-const sequelize = new Sequelize("teste" , "root" , senhamysql , {
-    host: "localhost" ,
-    dialect: "mysql" 
+//Atribuindo a função express a constante app
+const app  = express()
+
+//Config
+
+//Rotas
+
+//Outros
+
+const PORT = 8081
+app.listen(PORT , function(){
+    console.log('Servidor rodando')
 })
-*/
