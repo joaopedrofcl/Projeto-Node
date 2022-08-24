4d21a7903ee5362afc10b11b2b1725a6c28fc40c
João Pedro Fernandes <joaopedrofclol@gmail.com>
2022-08-24 17:40:02 -0300

    Update Categoria.js

--- models/Categoria.js
+++ models/Categoria.js
@@ -2,17 +2,18 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const Categoria = new Schema({
-    nome:{
+    nome: {
         type: String,
         required: true
     },
-    slug:{
+    slug: {
         type: String,
         required: true
     },
-    date:{
+    date: {
         type: Date,
         Default: Date.now()
+
     }
 })
 
